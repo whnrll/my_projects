c982fd703572a5ef9be898b24a8415ce8cefc3f4
xutao <xutao001225@163.com>
2023-04-09 00:13:36 +0800

    增加测试代码

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,21 @@
 package com.hmdp;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HmDianPingApplicationTests {
+public class HmDianPingApplicationTests {
+    @BeforeAll
+    public static void beforeAl() {}
+
+    @AfterAll
+    public static void afterAl() {}
+    @Test
+    @DisplayName("contextLoad")
+    public void contextLoad() {}
 
 
 }
